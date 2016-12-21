379089b689f7c6b5db201cabf74e63aab30f9717
Ainor Syahrizal <macintorch@gmail.com>
2016-12-21 16:09:25 +0800

    initial commit

---
+++ app/src/main/java/ainor/com/my/downloadwebcontent/MainActivity.java
@@ -0,0 +1,15 @@
+package ainor.com.my.downloadwebcontent;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
